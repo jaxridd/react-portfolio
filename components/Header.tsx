d1f173ba8fe0cbb00ea4a7fe79e213ccf050759f
jaxridd <jayriddell04@gmail.com>
2023-06-02 15:34:58 -0500

    Revert "Revert "Created Contact, Projects, and Skills Sections""
    This reverts commit 22c6157ef1a6769231b8c70f3a0d469e22b59bac.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
 import { motion } from "framer-motion"
+import Link from 'next/link';
 
 type Props = {}
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,16 @@ import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import WorkExperience from '@/components/WorkExperience'
+import Skills from '@/components/Skills';
+import Projects from '@/components/Projects';
+import ContactMe from '@/components/ContactMe';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory 
-    overflow-scroll z-0'>
+    overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20
+    scrollbar-thumb-[#F7AB0A]/80'>
       <Head>
         <title>Jax's Portfolio</title>
       </Head>
@@ -23,16 +28,23 @@ const Home: NextPage = () => {
         <About />
       </section>
 
-      {/* Experience */}
       <section id="experience" className='snap-center'>
         <WorkExperience />
       </section>
 
-      {/* Skills */}
+      <section id='skills' className='snap-start'>
+        <Skills />
+      </section>
 
-      {/* Projects */}
+      <section id='projects' className='snap-start'>
+        <Projects />
+      </section>
 
       {/* Contact Me */}
+      <section id='contact' className='snap-start'>
+        <ContactMe />
+      </section>
+
     </div>
   );
 };
