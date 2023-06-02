fab235d53eddd5e71cd09b65de34ebb08af1d8e1
jaxridd <jayriddell04@gmail.com>
2023-06-02 16:09:12 -0500

    Sanity added schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,10 +1,11 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
-import blockContent from './schemas/blockContent'
-import category from './schemas/category'
-import post from './schemas/post'
-import author from './schemas/author'
+import pageInfo from './schemas/pageInfo'
+import experience from './schemas/experience'
+import skill from './schemas/skill'
+import social from './schemas/social'
+import project from './schemas/project'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent],
+  types: [pageInfo, skill, social, project, experience ],
 }
